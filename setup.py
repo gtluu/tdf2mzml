cd5e7db21c0ff1821f60ed94007200ab0542d9a4
unknown <gtluu912@gmail.com>
2022-05-04 19:57:24 -0700

    make tdf2mzml package

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='tdf2mzml',
     version='0.3.20200211.1',
+    packages=['tdf2mzml'],
     url='https://github.com/mafreitas/tdf2mzml',
 )
---
+++ tdf2mzml/__init__.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sqlite3
+from ctypes import *
+import argparse
+import collections
+import hashlib
+import logging
+import math
+import os
+from psims.mzml import MzMLWriter
+import re
+import sqlite3, sys, time
+import sys
+import time
+
+from tdf2mzml.timsdata import *
+from tdf2mzml.tdf2mzml import *
